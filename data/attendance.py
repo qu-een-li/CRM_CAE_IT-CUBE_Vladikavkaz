d19ae74b7dc598949c345dd3a9d26b69602c9c71
under_construct <iartgametop@gmail.com>
2026-03-29 13:01:34 +0300

    исправлено название students_id_in_groups на student_id в attendance.py

--- data/attendance.py
+++ data/attendance.py
@@ -6,7 +6,7 @@ class Attendance(SqlAlchemyBase):
     __tablename__ = 'attendance'
     id = sq.Column(sq.Integer, primary_key=True,
                    autoincrement=True, nullable=False)
-    students_in_groups_id = sq.Column(
+    student_id = sq.Column(
         sq.Integer, sq.ForeignKey('students.id'), nullable=False)
     schedule_id = sq.Column(sq.Integer, sq.ForeignKey(
         'schedule.id'), nullable=False)
