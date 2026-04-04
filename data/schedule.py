96da8bedb5a60d0f9c1e5358f81aa66ab27bbb4d
Makarenkomd <mahadrum@yandex.ru>
2026-04-04 21:23:44 +0300

    Update schedule.py

--- data/schedule.py
+++ data/schedule.py
@@ -4,10 +4,8 @@ from .db_session import SqlAlchemyBase
 
 class Schedule(SqlAlchemyBase):
     __tablename__ = 'schedule'
-    id = sq.Column(sq.Integer, primary_key=True,
-                   autoincrement=True, nullable=False)
-    group_id = sq.Column(sq.Integer, sq.ForeignKey(
-        'groups.id'), nullable=False)
+    id = sq.Column(sq.Integer, primary_key=True, autoincrement=True, nullable=False)
+    group_id = sq.Column(sq.Integer, sq.ForeignKey('groups.id'), nullable=False)
     date = sq.Column(sq.Date, nullable=False)
     start_time = sq.Column(sq.Time, nullable=False)
     end_time = sq.Column(sq.Time, nullable=False)
