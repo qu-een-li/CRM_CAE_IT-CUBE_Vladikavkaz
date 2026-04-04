bbc3b7db637f94e05d1827e953238431d24f3ef7
qu-een-li <qu_een_li@icloud.com>
2026-04-04 14:17:59 +0300

    возвращение

--- application.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from config import KEY_CSRF
-from flask_login import LoginManager
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = KEY_CSRF
-
-login_manager = LoginManager()
--- funcs.py
+++
@@ -1,5 +0,0 @@
-from data.teacher import Teacher
-
-
-def get_full_teachers_initials_by_column(teacher: Teacher):
-    return f'{teacher.name} {teacher.patronymic} {teacher.surename}'
