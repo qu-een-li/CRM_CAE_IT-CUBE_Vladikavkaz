0d2e0a05ec8b0fae6fe2dc34373f6c4cf97b4c23
under_construct <iart.game@yandex.ru>
2026-04-10 16:19:50 +0300

    заглушка на get_more_jobs

--- routes/schedule.py
+++ routes/schedule.py
@@ -49,41 +49,42 @@ def show_schedules():
 
 @app.route("/get_more_days")
 def get_more_days():
-    start_date_str = request.args.get("start_date")
-    first_day_of_week = datetime.strptime(start_date_str, "%Y-%m-%d")
-    first_day_of_week -= timedelta(days=first_day_of_week.weekday())
-    last_day_of_week = first_day_of_week + timedelta(days=6)
-    weeks = []
-    db_sess = create_session()
-    schedules = db_sess.query(Schedule).options(joinedload(Schedule.group)).all()
-    schedules.sort(key=lambda x: (x.start_time, x.end_time))
-    duration_keys = set()
-    for _ in range(2):
-        week = []
-        for day_of_week in range(7):
-            day = {}
-            current_day = last_day_of_week + timedelta(days=day_of_week)
-            for schedule in schedules:
-                if schedule.is_schedule_at_date(current_day):
-                    duration_str = f"{schedule.start_time.strftime('%H:%M')}-{schedule.end_time.strftime('%H:%M')}"
-                    if duration_str in day:
-                        day[duration_str].append(schedule)
-                    else:
-                        day[duration_str] = [schedule]
-                        duration_keys.add(duration_str)
-            week.append(day)
-        matrix = []
-        row = []
-        for duration in duration_keys:
-            max_len_of_duration = 0
-            for day in week:
-                if duration in day:
-                    if len(day[duration]) > max_len_of_duration:
-                        max_len_of_duration = len(day[duration])
-            row.append((duration_str, max_len_of_duration))
-        matrix.append(row)
-        for day in week:
+    # start_date_str = request.args.get("start_date")
+    # first_day_of_week = datetime.strptime(start_date_str, "%Y-%m-%d")
+    # first_day_of_week -= timedelta(days=first_day_of_week.weekday())
+    # last_day_of_week = first_day_of_week + timedelta(days=6)
+    # weeks = []
+    # db_sess = create_session()
+    # schedules = db_sess.query(Schedule).options(joinedload(Schedule.group)).all()
+    # schedules.sort(key=lambda x: (x.start_time, x.end_time))
+    # duration_keys = set()
+    # for _ in range(2):
+    #     week = []
+    #     for day_of_week in range(7):
+    #         day = {}
+    #         current_day = last_day_of_week + timedelta(days=day_of_week)
+    #         for schedule in schedules:
+    #             if schedule.is_schedule_at_date(current_day):
+    #                 duration_str = f"{schedule.start_time.strftime('%H:%M')}-{schedule.end_time.strftime('%H:%M')}"
+    #                 if duration_str in day:
+    #                     day[duration_str].append(schedule)
+    #                 else:
+    #                     day[duration_str] = [schedule]
+    #                     duration_keys.add(duration_str)
+    #         week.append(day)
+    #     matrix = []
+    #     row = []
+    #     for duration in duration_keys:
+    #         max_len_of_duration = 0
+    #         for day in week:
+    #             if duration in day:
+    #                 if len(day[duration]) > max_len_of_duration:
+    #                     max_len_of_duration = len(day[duration])
+    #         row.append((duration_str, max_len_of_duration))
+    #     matrix.append(row)
+    #     for day in week:
 
-    next_date = (last_day_of_week + timedelta(days=1)).strftime("%Y-%m-%d")
+    # next_date = (last_day_of_week + timedelta(days=1)).strftime("%Y-%m-%d")
 
-    return render_template("show_schedules_batch.html", next_date=next_date)
+    # return render_template("show_schedules_batch.html", next_date=next_date)
+    return ""
