3584233a1d4112e569b126d36f8c7adff78816df
under_construct <iart.game@yandex.ru>
2026-04-19 14:56:57 +0300

    добавлена страница ошибки

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from app import app
 from data import db_session
-from routes import main, students, search, schedule, teachers, contests, groups
+from routes import main, students, search, schedule, teachers, contests, groups, error_handlers
 import os
 from config import UPLOAD_FOLDER
 import locale
@@ -12,4 +12,4 @@ if __name__ == "__main__":
     os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
     db_session.global_init("db/reg_form.db")
-    app.run(port=8080, host="127.0.0.1")
+    app.run(port=8080, host="127.0.0.1", debug=True)
