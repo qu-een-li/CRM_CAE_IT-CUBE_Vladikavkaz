6fb56800ae15c1d8d16f354426e9ce0351300d4c
Nika <nikusik.bratusik@yandex.ru>
2026-04-30 00:20:19 +0300

    можно просмотреть информацию о конкурсе (название, дату и организматора)

--- routes/teachers_contests.py
+++ routes/teachers_contests.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from data import db_session
 from data.contest_for_teachers import Contest_for_Teachers
 from datetime import date
@@ -11,3 +11,13 @@ def teachers_contests_list():
     contests = db_sess.query(Contest_for_Teachers).all()
     today = date.today()
     return render_template("teachers_contests.html", contests=contests, today=today, date=date)
+
+
+@app.route("/teachers_contests/<int:contest_id>")
+def teachers_contest_details(contest_id):
+    db_sess = db_session.create_session()
+    contest = db_sess.query(Contest_for_Teachers).get(contest_id)
+    if not contest:
+        flash("Конкурс не найден", "danger")
+        return redirect(url_for("contests_list"))
+    return render_template("teachers_contest_details.html", contest=contest)
