2419bdbebfb363fd9f83c6371bb01fead588e719
Nika <nikusik.bratusik@yandex.ru>
2026-05-08 20:38:11 +0300

    изменение информации о конкурсе

--- routes/teachers_contests.py
+++ routes/teachers_contests.py
@@ -1,4 +1,5 @@
 from flask import render_template, flash, redirect, url_for, request
+
 from data import db_session
 from data.contest_for_teachers import Contest_for_Teachers
 from data.level_contest import Level_contest
@@ -60,3 +61,46 @@ def add_teacher_contest():
                            levels=levels,
                            title='Добавить конкурс',
                            contest=None)
+
+
+@app.route('/edit_teacher_contest/<int:contest_id>', methods=['GET', 'POST'])
+def edit_teacher_contest(contest_id):
+    db_sess = db_session.create_session()
+    contest = db_sess.query(Contest_for_Teachers).get(contest_id)
+    if not contest:
+        flash('Конкурс не найден', 'danger')
+        return redirect(url_for('teachers_contests_list'))
+
+    levels = db_sess.query(Level_contest).all()
+
+    if request.method == 'POST':
+        contest.name = request.form.get('name')
+        start_date = request.form.get('start_date')
+        if start_date:
+            try:
+                contest.date = datetime.strptime(start_date, '%d.%m.%Y').date()
+            except ValueError:
+                contest.date = datetime.strptime(start_date, '%Y-%m-%d').date()
+
+        end_date = request.form.get('end_date')
+        if end_date:
+            try:
+                contest.end_date = datetime.strptime(end_date, '%d.%m.%Y').date()
+            except ValueError:
+                contest.end_date = datetime.strptime(end_date, '%Y-%m-%d').date()
+        else:
+            contest.end_date = None
+
+        contest.link_contest = request.form.get('link_contest')
+        contest.description = request.form.get('description')
+        contest.level_id = int(request.form.get('level_id'))
+        contest.organizer = request.form.get('contest_organizer')
+
+        db_sess.commit()
+        flash('Конкурс успешно обновлен!', 'success')
+        return redirect(url_for('teachers_contest_details', contest_id=contest.id))
+
+    return render_template('add_edit_teacher_contest.html',
+                           levels=levels,
+                           title='Редактировать конкурс',
+                           contest=contest)
