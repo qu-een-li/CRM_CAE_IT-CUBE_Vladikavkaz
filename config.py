25eddbd6d872709002eda2c47c986fb8b483e9b2
under_construct <iart.game@yandex.ru>
2026-05-13 23:02:00 +0300

    изменение host

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ KEY_CSRF = "yandexlyceum_secret_key"
 KEY_API = "AizdvTuXgfoBlLspQmBPdRuVAaKbsGZk"
 UPLOAD_FOLDER = os.path.join("static", "uploads", "teachers")
 MAX_CONTENT_LENGTH = 16 * 1024 * 1024
-PORT, HOST = 5001, "127.0.0.1"
+PORT, HOST = 5001, "0.0.0.0"
 API_PORT, API_HOST = 5002, "127.0.0.1"
